5cec898303663f3666ee40140cc6d51b49f75bd2
elly_touami <eltouami@ucdavis.edu>
2026-03-10 13:42:29 -0700

    game-html

---
+++ game/v1-html-css-only/script.js
@@ -0,0 +1,10 @@
+( function() {
+    'use strict';
+    console.log('reading JS');
+
+    // change background
+    // const backgrnd = document.body;
+    
+    // backgrnd.style.backgroundImage = "url('images/p2\ background.png')";
+
+}());
